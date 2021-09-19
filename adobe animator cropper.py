7d2f0fe0e99f2c80fd632397fcb3160e7106e542
nzin4x <nzin4x@gmail.com>
2021-09-20 07:50:58 +0900

    change variable name

--- adobe animator cropper.py
+++ adobe animator cropper.py
@@ -28,8 +28,8 @@ for idx in range(len(defs)):
     iDef = defs[idx]
     name = iDef["name"]
     
-    height = int(iDef["height"])
-    width = int(iDef["width"])
+    h = int(iDef["height"])
+    w = int(iDef["width"])
     x = int(iDef["x"])
     y = int(iDef["y"])
     try:
@@ -38,43 +38,45 @@ for idx in range(len(defs)):
         fx = int(iDef["frameX"])
         fy = int(iDef["frameY"])
     except:
-        fw = width
-        fh = height
+        fw = w
+        fh = h
         fx = 0
         fy = 0
     # print("name : ", name, " width : ", width)
 
-    #if idx < 58:
-    #    continue
+    # if idx < 58:
+    #     continue
 
-    #if 'NOTE DOWN' not in name :
-    #   continue
+    if 'NOTE DOWN' not in name:
+        continue
 
     if idx >= 1 :
-        if defs[idx - 1]['height'] == defs[idx - 0]['height'] and defs[idx - 1]['width'] == defs[idx - 0]['width'] and defs[idx - 1]['y'] == defs[idx - 0]['y']  and defs[idx - 1]['x'] == defs[idx - 0]['x']:
+        if defs[idx - 1]['height'] == defs[idx - 0]['height'] and defs[idx - 1]['width'] == defs[idx - 0]['width'] \
+                and defs[idx - 1]['y'] == defs[idx - 0]['y'] and defs[idx - 1]['x'] == defs[idx - 0]['x']:
             print('같은 이미지 버리기 name : ', name)
             continue
 
     org = Image.open(imgfile)
 
-    cropped = org.crop((x, y, x + width, y + height))
+    cropped = org.crop((x, y, x + w, y + h))
 
-    crop_img_path = os.path.join(base, filename + "_crop", name + '.png')
+    canvas_img_path = os.path.join(base, filename + "_crop", 'canvas ' + name + '.png')
+    tight_img_path = os.path.join(base, filename + "_crop", 'tight ' + name + '.png')
     
     new_canvas = (fw, fh)
     new_image = Image.new("RGBA", new_canvas)
     
-    print("name : ", name, " width : ", width, " height[",height,"], x[",x,"], y[",y,"], fx[",fx,"], fy[",fy,"], fw[",fw,"], fh[",fh,"]")
+    print("name : ", name, " width : ", w, " height[", h, "], x[", x, "], y[",
+          y, "], fx[", fx, "], fy[", fy, "], fw[", fw, "], fh[", fh, "]")
 
-    if fx >=0 :
-        fx = 0
-    if fy >=0 :
-        fy = 0
+    # if fx > 0:
+    #     fx = 0
+    # if fy > 0:
+    #     fy = 0
 
-    new_image.paste(cropped, (-fx, -fy))
-    new_image.save(crop_img_path)
-    #cropped.save(crop_img_path)
+    new_image.paste(cropped, (fw - w + fx, fh - h + fy))
+    new_image.save(canvas_img_path)
+    # cropped.save(tight_img_path)
 
-
-    #org.border(Color('transparent'), int((460 - width) / 2), int((460 - height) / 2) )
-    #org.save(filename = os.path.join(base, 'crop', 'b' + name + '.png'))
+    # org.border(Color('transparent'), int((460 - w) / 2), int((460 - h) / 2) )
+    # org.save(filename = os.path.join(base, 'crop', 'b' + name + '.png'))
